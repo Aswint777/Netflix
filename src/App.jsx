5793d4963574bb3c0fd9a97f6dbd4b3cdcf2b2d0
AswinT <aswint2018@gmail.com>
2024-05-27 18:11:53 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./assets/pages/Home";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
-
   return (
     <>
-     <h1>hloooo</h1>
+      <BrowserRouter>
+        <AuthContextProvider>
+          <Navbar />
+
+          <Routes>
+
+            <Route path="/" element={<Home />} />
+
+          </Routes>
+        </AuthContextProvider>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/firebase.jsx
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+  authDomain:import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN ,
+  projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MASSAGING_SENDER,
+  appId: import.meta.env.VITE_REACT_APP_APP_ID
+};
+
+
+// REACT_APP_FIREBASE_API_KEY =AIzaSyDgTsCdXJhM-msHeA4TYGqXAjrvPKNduYM
+// REACT_APP_FIREBASE_AUTH_DOMAIN =netflix-react-yt-63dcf.firebaseapp.com
+// REACT_APP_FIREBASE_PROJECT_ID =netflix-react-yt-63dcf
+// REACT_APP_FIREBASE_STORAGE_BUCKET =netflix-react-yt-63dcf.appspot.com
+// REACT_APP_FIREBASE_MASSAGING_SENDER =201035631863
+// REACT_APP_APP_ID =1:201035631863:web:796d9a36f30ba23bdc97f2
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
